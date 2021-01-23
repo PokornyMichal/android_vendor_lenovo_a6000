b02a053e7d3050968e59c375041fb4af6e348f40
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-23 10:44:58 +0100

    a6000: Ship and use v27 android.frameworks.sensorservice@1.0.so for slim_daemon
    * Current one is missing _ZN7android10frameworks13sensorservice4V1_08toStringENS2_6ResultE
    
    Change-Id: Ib38ce488509e0bb8dc42184883bbe773b6bb26bf

--- a6000-vendor.mk
+++ a6000-vendor.mk
@@ -290,7 +290,8 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/a6000/proprietary/vendor/lib/libxtadapter.so:system/vendor/lib/libxtadapter.so \
     vendor/lenovo/a6000/proprietary/vendor/lib/libxtwifi_ulp_adaptor.so:system/vendor/lib/libxtwifi_ulp_adaptor.so \
     vendor/lenovo/a6000/proprietary/vendor/lib/vendor.qti.gnss@1.0_vendor.so:system/vendor/lib/vendor.qti.gnss@1.0_vendor.so \
-    vendor/lenovo/a6000/proprietary/system/lib/android.hardware.gnss@1.0-v27.so:system/lib/android.hardware.gnss@1.0-v27.so
+    vendor/lenovo/a6000/proprietary/system/lib/android.hardware.gnss@1.0-v27.so:system/lib/android.hardware.gnss@1.0-v27.so \
+    vendor/lenovo/a6000/proprietary/system/lib/android.frameworks.sensorservice@1.0-v27.so:system/lib/android.frameworks.sensorservice@1.0-v27.so
 
 PRODUCT_PACKAGES += \
     shutdownlistener \
